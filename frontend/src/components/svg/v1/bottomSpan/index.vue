5b2a51b78f4515ce84d4c43724aae272564b2fda
Pedro <laranjeiraspedro@gmail.com>
2021-09-02 21:50:17 -0300

    add svg vue

---
+++ frontend/src/components/svg/v1/bottomSpan/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" viewBox="0 0 193 2">
+    <g transform="translate(-91.5 -164)">
+      <line
+        x2="85"
+        transform="translate(199.5 164.5)"
+        fill="none"
+        :stroke="stroke"
+        stroke-width="1"
+      />
+      <line
+        x2="85"
+        transform="translate(91.5 164.5)"
+        fill="none"
+        :stroke="stroke"
+        stroke-width="1"
+      />
+      <circle cx="1" cy="1" r="1" transform="translate(187 164)" :fill="fill" />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: { type: Number || String, default: 193 },
+    height: { type: Number || String, default: 2 },
+    fill: { type: String, default: "#fff" },
+    stroke: { type: String, default: "#fbfbfb" }
+  }
+};
+</script>
